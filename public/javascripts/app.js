8b59bcdafda01f7264c4b6e469b32258465ebc5b
Trevor Kimenye <trevor@kimenye.com>
2012-05-19 01:33:07 +1000

    Added effect for showing and hiding

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -55,12 +55,63 @@ function App() {
         self.categories.push(new ComparableItemCategory({ type: ComparableCategoryType.CAR_LOANS, title: "Car Loans", icon: "/images/carloans.png" }));
 
         console.log("Number of categories loaded " + self.categories().length);
-    }
+    };
 
-    //self.init = function() {
+
+
+    self.selectCategory = function(category) {
+//        self.selectedCategory(category)
+//        console.log("Selected type " + category.title());
+        location.hash = category.type ()
+    };
+
     self.loadCategories();
-    //}
+
+    Sammy(function() {
+
+        this.get('#:category', function() {
+            console.log("Params is " + this.params.category);
+
+
+            var category = ko.utils.arrayFilter(self.categories, function (category) {
+                return category.type() == this.params.category;
+            });
+            if (category)
+                self.selectedCategory(category);
+
+        });
+
+        this.get('', function() { console.log("In the root url ") } );
+
+//        this.get('', function() { this.app.runRoute('get', '#credit-cards') });
+
+    }).run();
 }
 
-ko.applyBindings(new App());
+
+//function CarLoanWizard() {
+//    var self = this;
+//}
+
+ko.bindingHandlers.fadeVisible = {
+    init: function(element, valueAccessor) {
+        // Initially set the element to be instantly visible/hidden depending on the value
+        var value = valueAccessor();
+        $(element).toggle(ko.utils.unwrapObservable(value)); // Use "unwrapObservable" so we can handle values that may or may not be observable
+    },
+    update: function(element, valueAccessor) {
+        // Whenever the value subsequently changes, slowly fade the element in or out
+        var value = valueAccessor();
+        ko.utils.unwrapObservable(value) ? $(element).fadeIn("slow") : $(element).fadeOut("slow");
+    }
+};
+
+$(function() {
+    console.log("About to load bindings");
+    ko.applyBindings(new App());
+});
+
+
+
+
 
